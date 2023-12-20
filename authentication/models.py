46e726c9fd4761fe9f3f6290f30d3be73a8e1385
Goku <ayushkunwar167@gmail.com>
2023-12-20 19:39:12 +0530

    Added notes model

--- authentication/models.py
+++ authentication/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from base.models import BaseModel
 from django.contrib.auth.models import User
 
+
+class Note(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    content = models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self) -> str:
+        return self.user.username
+
 class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     is_email_verified = models.BooleanField(default=False)
     email_token = models.CharField(max_length=100, null=True, blank=True)
     profile_image = models.ImageField(upload_to="images/_profiles",default="images/_profiles/default.png")
-    # def __str__(self) -> str:
-    #     return self.user
+    def __str__(self) -> str:
+        return self.user.username
