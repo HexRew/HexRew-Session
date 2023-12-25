eedb8ac314ad074992bda0ccefc9e5e4dd51c683
R0h1th-1DD4E2 <rohith.010404@gmail.com>
2023-12-25 18:46:23 +0530

    Navbar Done, Getting back to Bootstrap

--- authentication/models.py
+++ authentication/models.py
@@ -25,6 +25,6 @@ class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     is_email_verified = models.BooleanField(default=False)
     email_token = models.CharField(max_length=100, null=True, blank=True)
-    profile_image = models.ImageField(upload_to="images/_profiles",default="images/_profiles/default.png")
+    profile_image = models.ImageField(upload_to="images/_profiles",default="images/_profiles/default_usr_icon.png")
     def __str__(self) -> str:
         return self.user.username
